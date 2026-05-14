7e1c395df3fcddb877916c2e36e5717aaa973e21
shadab <shadabbgs3@gmail.com>
2026-05-14 11:02:48 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿// this will act as a bridge between application and database 
+// It is responsible for DB communication  
+// it tracks entity changes 
+
+using Microsoft.EntityFrameworkCore;
+using EFCoreDemo.Models;
+
+namespace EFCoreDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> students { get; set; }
+
+        // above property is used for accessing student class members as properties
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+namespace EFCoreDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
